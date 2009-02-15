9af3a59bbbb873fd7a0a2c7b77567e1b032f7874
jfifield <jfifield>
2009-02-15 15:15:51 +0000

    Moving JSch support using JSchConnectionManager.

--- bin/sshtunnel.sh
+++ bin/sshtunnel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-CLASSPATH="j2ssh-core.jar;j2ssh-common.jar;commons-logging.jar;jdic.jar;windows\jdic_stub.jar;sshtunnel.jar"
+CLASSPATH="jsch-0.1.37.jar;j2ssh-core.jar;j2ssh-common.jar;commons-logging.jar;jdic.jar;windows\jdic_stub.jar;sshtunnel.jar"
 java -cp $CLASSPATH -Djava.library.path=linux org.programmerplanet.sshtunnel.Main
