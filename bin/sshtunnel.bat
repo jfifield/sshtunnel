3695953f25534d0d26fac984d2519332089f269b
jfifield <jfifield>
2009-02-16 19:21:23 +0000

    changing project to maven 2 build

--- bin/sshtunnel.bat
+++
@@ -1,2 +0,0 @@
-set CLASSPATH="jsch-0.1.37.jar;j2ssh-core.jar;j2ssh-common.jar;commons-logging.jar;sshtunnel.jar"
-javaw -cp %CLASSPATH% -Djava.library.path=windows org.programmerplanet.sshtunnel.ui.swing.Main
