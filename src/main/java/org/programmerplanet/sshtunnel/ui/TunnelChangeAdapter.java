75cd91c8e1ca7905bb7bc1e9fb4be3bef09cfd6c
jfifield <jfifield>
2009-02-21 13:50:40 +0000

    Updating copyright.

--- src/main/java/org/programmerplanet/sshtunnel/ui/TunnelChangeAdapter.java
+++ src/main/java/org/programmerplanet/sshtunnel/ui/TunnelChangeAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 Joseph Fifield
+ * Copyright 2009 Joseph Fifield
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
